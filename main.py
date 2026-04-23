5f2d38d27e353efd4e621922e4b0b58ec4688d05
risaoya <rrrisayayama0911@gmail.com>
2026-04-24 07:51:51 +0900

    2つの数字を足し算する２

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
+# 2つの数字を足し算する
 
-#2つの数字を足し算する
-
-
+"""
 while True:
-    a =input("1つ目の数字：")
-    b=input("2つ目の数字：")
 
-    if not a.isdigit() or not b.isdigit():
-        print("数字を入力してね")
-        continue
-    #inputは文字列より、int()関数で数値や文字列を整数型に変換する
-    a =int(a)
-    b=int(b)
- 
-    result=a+b
-    print("結果：",result)
+    try:
+        a = int(input("1つ目の数字："))
+        b = int(input("2つ目の数字："))
+        break
 
-    break
+    except ValueError:
+        print("数字を入力してね:")
 
+result = a + b
+print("結果：", result)
+
+
+"""
 
 while True:
     try:
@@ -26,7 +24,7 @@ while True:
         a=int(input("1つ目の数字："))
         b=int(input("2つ目の数字："))
         break
-    except:
+    except ValueError:
         print("数字を入力してね")
 
 result=a+b
